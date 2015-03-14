485e57351709dd85176ad075bc74363be747821e
Michael Marshall <mtmarsh2@illinois.edu>
2015-03-13 20:11:06 -0500

    Added models file

--- CrowdML/jobs/models.py
+++ CrowdML/jobs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Job(models.Model):
+    user = models.ForeignKey(User)
+
+class Item(models.Model):
+    url = models.Charfield(max_length = 200)
+    label = models.Charfield(max_length = 200)
+    job = models.ForeignKey(Job)
+
+class Score(models.Model):
+    value = models.Integerfield()
+    item = models.ForeignKey(Item)
