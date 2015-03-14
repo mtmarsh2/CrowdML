c3fdd08d4926e324ba0695200e69d51a9a31c282
Michael Marshall <mtmarsh2@illinois.edu>
2015-03-13 21:33:53 -0500

    added dropbox link taking

--- CrowdML/jobs/views.py
+++ CrowdML/jobs/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from bs4 import BeautifulSoup
+import urllib2
+
+#gets list of urls from dropbox url
+def get_imagelinks_from_dropbox(url):
+    links = []
+    response = urllib2.urlopen(url)
+    soup = BeautifulSoup(response)
+    section = soup.find('ol', id='gallery-view-media')
+    lis = section.find_all('a')
+    for li in lis:
+        links.append(li['href'][:-5])
+    return links
